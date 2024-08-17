f5bdc7ddd4c596014359f00f2358d58269726233
shabbeer <mdshabbeer707@gmail.com>
2024-08-17 17:25:23 +0530

    home page done

--- src/Component/Banner/Banner.js
+++ src/Component/Banner/Banner.js
@@ -58,6 +58,28 @@ const Banner = () => {
                         speed={1500}
                         navigation={true}
                         modules={[Autoplay, Navigation]}
+                        breakpoints={{
+                            // when window width is >= 320px
+                            320: {
+                                slidesPerView: 2,
+                                spaceBetween: 10,
+                            },
+                            // when window width is >= 480px
+                            480: {
+                                slidesPerView: 3,
+                                spaceBetween: 15,
+                            },
+                            // when window width is >= 768px
+                            768: {
+                                slidesPerView: 4,
+                                spaceBetween: 20,
+                            },
+                            // when window width is >= 1024px
+                            1024: {
+                                slidesPerView: 5,
+                                spaceBetween: 20,
+                            },
+                        }}
                     >
                         <div className="banner-items">
                             {BannerItem.map((item) => (
--- src/Component/HCategory/HCategory.js
+++ src/Component/HCategory/HCategory.js
@@ -22,14 +22,32 @@ const HCategory = () => {
                         className="mySwiper"
                         slidesPerView={4}
                         spaceBetween={20}
-                        // autoplay={{
-                        //     delay: 3000,
-                        //     disableOnInteraction: false,
-                        // }}
                         loop={true}
                         speed={1000}
                         navigation={true}
                         modules={[Autoplay, Navigation]}
+                        breakpoints={{
+                            // when window width is >= 320px
+                            320: {
+                                slidesPerView: 1,
+                                spaceBetween: 10,
+                            },
+                            // when window width is >= 480px
+                            480: {
+                                slidesPerView: 2,
+                                spaceBetween: 15,
+                            },
+                            // when window width is >= 768px
+                            768: {
+                                slidesPerView: 3,
+                                spaceBetween: 20,
+                            },
+                            // when window width is >= 1024px
+                            1024: {
+                                slidesPerView: 4,
+                                spaceBetween: 20,
+                            },
+                        }}
                     >
                         {HCategoryData.map((item) => (
                             <SwiperSlide key={item.id}>
